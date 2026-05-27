4dfbd70e48005712d30fab340d7a38c25fb08b23
Shirish Koirala <shirishkoirala@gmail.com>
2026-05-27 14:57:40 +1000

    Use Inter as UI font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import '@fontsource-variable/inter';
 import './styles/tokens.css';
 import './styles/global.css';
 
